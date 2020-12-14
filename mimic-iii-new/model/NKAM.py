91403ed920858e1a148d5d8241139196a680c5ca
masaka_mac <438049678@qq.com>
2020-12-14 16:59:11 +0800

    1214-mac

--- mimic-iii-new/model/NKAM.py
+++ mimic-iii-new/model/NKAM.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 import tensorflow.keras as keras
+from tensorflow.keras import backend as K
 from tensorflow.keras.callbacks import Callback
 import _pickle as pickle
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import heapq
 import operator
 import os
-
 
 _TEST_RATIO = 0.15
 _VALIDATION_RATIO = 0.1
@@ -127,66 +129,77 @@ def process_label(labelSeqs):
     return newlabelSeq
 
 
-class KAMEAttention(keras.Model):
-    def __init__(self, units):
-        super(KAMEAttention, self).__init__()
-        self.W1 = keras.layers.Dense(units)
-        # self.W2 = keras.layers.Dense(units)
-        # self.V = keras.layers.Dense(1)
+class MyEmbedding(keras.layers.Layer):
+    def __init__(self,embedding_matrix, **kwargs):
+        self.embedding_matrix = embedding_matrix
+        super(MyEmbedding, self).__init__(**kwargs)
 
-    # tree_onehot, rnn_ht
+    def build(self, input_shape):
+        # assert isinstance(input_shape, list)
+        # Create a trainable weight variable for this layer.
+        self.kernel = self.add_weight(name='embedding_matrix',
+                                      shape=(self.embedding_matrix.shape[0], self.embedding_matrix.shape[1]),
+                                      initializer=keras.initializers.constant(self.embedding_matrix),
+                                      trainable=True)
+        super(MyEmbedding, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, inputs):
+        # assert isinstance(x, list)
+        emb = K.tanh(K.dot(inputs, self.kernel))
+        return emb
+
+
+class ScaledDotProductAttention(keras.layers.Layer):
+    def __init__(self, output_dim, **kwargs):
+        # inputs.shape = (batch_size, time_steps, seq_len)
+        self.output_dim = output_dim
+        super(ScaledDotProductAttention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # 为该层创建一个可训练的权重
+        # inputs.shape = (batch_size, time_steps, seq_len)
+        self.kernel = self.add_weight(name='kernel',
+                                      shape=(2, input_shape[0][2], self.output_dim),
+                                      initializer='uniform',
+                                      trainable=True)
+        self.W = self.add_weight(name='W',
+                                 shape=(input_shape[1][2], self.output_dim),
+                                 initializer='uniform',
+                                 trainable=True)
+
+        super(ScaledDotProductAttention, self).build(input_shape)  # 一定要在最后调用它
+
     def call(self, inputs):
         Lt, rnn_ht = inputs
-        # Lt.shape:(batch_size, length, size, units)=>(5250,41,84,128)
-        # rnn_ht.shape:(batch_size, length, units) =>(5250,41,128)
+        WQ = K.dot(rnn_ht, self.W)
+        WK = K.dot(Lt, self.kernel[0])
+        WV = K.dot(Lt, self.kernel[1])
+        # WQ.shape (None, 41, 128)
+        # print("WQ.shape", WQ.shape)
+        # 转置 K.permute_dimensions(WK, [0, 2, 1]).shape (None, 128, 41)
+        # print("K.permute_dimensions(WK, [0, 2, 1]).shape", K.permute_dimensions(WK, [0, 2, 1]).shape)
 
-        # ht.shape:(batch_size, length, size, units)=>(5250,41,1,128)
-        ht = tf.expand_dims(rnn_ht, 2)
-        Lt = self.W1(Lt)
-        # score.shape:(batch_size, length, size, 1)
-        score = tf.multiply(ht, Lt)
+        QK = K.batch_dot(WQ, K.permute_dimensions(WK, [0, 2, 1]))
 
-        # (Alpha)attention_weights.shape:(batch_size, length, size, 1)
-        attention_weights = tf.nn.softmax(score, axis=2)
+        QK = QK / (64 ** 0.5)
 
-        # context_vector.shape: (batch_size, length, size, units)
-        # tensorflow自己做扩展，把Alpha的1扩展为units
-        context_vector = attention_weights * Lt
+        weights = K.softmax(QK)
+        # QK.shape (None, 41, 41)
+        # print("QK.shape", weights.shape)
 
-        # 在size维度求和，context_vector.shape:(batch_size, length, units)
-        context_vector = tf.reduce_sum(context_vector, axis=2)
+        context_vector = K.batch_dot(weights, WV)
 
-        return context_vector
+        return context_vector, weights
 
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], input_shape[1], self.output_dim)
 
-def kame_knowledgematrix(treeseq_set, emb):
-    # 和患者输入保持一致，访问为1到n-1
-    for i in range(len(treeseq_set)):
-        treeseq_set[i] = treeseq_set[i][:-1]
-
-    zerovec = np.zeros((84, 728)).astype(np.float32)
-    ts = []
-    for i in treeseq_set:
-        count = 0
-        a = []
-        for j in i:
-            # 变为onehot
-            temp = keras.utils.to_categorical(j)
-            if len(temp) < 84:
-                zerovec1 = np.zeros((84-len(temp), 728)).astype(np.float32)
-                temp = np.r_[temp, zerovec1]
-            count += 1
-            a.append(temp)
-        while count < 41:
-            a.append(zerovec)
-            count += 1
-        ts.append(a)
-
-    for i in range(len(ts)):
-        for j in range(len(ts[i])):
-            ts[i][j] = np.matmul(ts[i][j], emb)
-
-    return np.array(ts)
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'output_dim': self.output_dim,
+        })
+        return config
 
 
 def visit_level_precision(y_true, y_pred, rank=[5]):
@@ -231,26 +244,25 @@ class metricsHistory(Callback):
         super().__init__()
         self.Recall_5 = []
         self.Precision_5 = []
-        # self.path = 'G:\\模型训练保存\\KAME_' + str(gru_dimentions) + '_dropout\\rate05\\'
-        # self.fileName = 'model_metrics.txt'
+        self.path = 'G:\\模型训练保存\\ourmodel_' + str(gru_dimentions) + '_dropout\\rate05_02\\'
+        self.fileName = 'model_metrics.txt'
 
     def on_epoch_end(self, epoch, logs={}):
-        # precision5 = visit_level_precision(process_label(test_set[1]), convert2preds(
-        #     model.predict([x_test, tree_test])))[0]
+        precision5 = visit_level_precision(process_label(test_set[1]), convert2preds(
+            model.predict([x_test, tree_test])))[0]
         recall5 = code_level_accuracy(process_label(test_set[1]),convert2preds(
             model.predict([x_test, tree_test])))[0]
-        # self.Precision_5.append(precision5)
+        self.Precision_5.append(precision5)
         self.Recall_5.append(recall5)
-        # metricsInfo = 'Epoch: %d, - Recall@5: %f, - Precision@5: %f' % (epoch+1, recall5, precision5)
-        metricsInfo = 'Epoch: %d, - Recall@5: %f' % (epoch + 1, recall5)
-        # print2file(metricsInfo, self.path, self.fileName)
+        metricsInfo = 'Epoch: %d, - Recall@5: %f, - Precision@5: %f' % (epoch+1, recall5, precision5)
+        print2file(metricsInfo, self.path, self.fileName)
         print(metricsInfo)
 
     def on_train_end(self, logs={}):
         print('Recall@5为:',self.Recall_5,'\n')
         print('Precision@5为:',self.Precision_5)
-        # print2file('Recall@5:'+str(self.Recall_5), self.path, self.fileName)
-        # print2file('Precision@5:'+str(self.Precision_5), self.path, self.fileName)
+        print2file('Recall@5:'+str(self.Recall_5), self.path, self.fileName)
+        print2file('Precision@5:'+str(self.Precision_5), self.path, self.fileName)
 
 
 def print2file(buf, dirs, fileName):
@@ -267,50 +279,40 @@ if __name__ == '__main__':
     labelFile = '../resource/mimic3.allLabels'
     treeFile = '../resource/mimic3_newTree.seqs'
 
-    gloveKnowledgeFile = './resource/embedding/glove_knowledge_test.npy'
-    gramembFile = './resource/embedding/gram_emb_final.npy'
+    # gramembFile = './resource/embedding/gram_emb_final.npy'
 
-    glove_knowledge_emb = np.load(gloveKnowledgeFile).astype(np.float32)
-    gram_emb = np.load(gramembFile).astype(np.float32)
+    # node2vec Embedding
+    diagcode_emb = np.load('../resource/node2vec_emb/diagcode_emb.npy')
+    knowledge_emb = np.load('../resource/node2vec_emb/knowledge_emb.npy')
 
-    # 测试tree的序列
-    # tree_seq = pickle.load(open(treeFile, 'rb'))
+    # gram Embedding
+    # diagcode_emb = np.load('../resource/xxx.npy')
 
     train_set, valid_set, test_set = load_data(seqFile, labelFile, treeFile)
-    x, y, lengths = padMatrix(train_set[0], train_set[1])
-    x_valid, y_valid, valid_lengths = padMatrix(valid_set[0], valid_set[1])
-    x_test, y_test, test_lengths = padMatrix(test_set[0], test_set[1])
-
-    # KAME knowledge embedding
-    tree = kame_knowledgematrix(train_set[2], glove_knowledge_emb)
-    tree_valid = kame_knowledgematrix(valid_set[2], glove_knowledge_emb)
-    tree_test = kame_knowledgematrix(test_set[2], glove_knowledge_emb)
-
-    # gram patient embedding
-    x = tf.matmul(x, tf.expand_dims(gram_emb, 0))
-    x_valid = tf.matmul(x_valid, tf.expand_dims(gram_emb, 0))
-    x_test = tf.matmul(x_test, tf.expand_dims(gram_emb, 0))
-
+    x, y, tree = padMatrix(train_set[0], train_set[1], train_set[2])
+    x_valid, y_valid, tree_valid = padMatrix(valid_set[0], valid_set[1], valid_set[2])
+    x_test, y_test, tree_test = padMatrix(test_set[0], test_set[1], test_set[2])
 
     gru_input = keras.layers.Input((x.shape[1], x.shape[2]), name='gru_input')
     mask = keras.layers.Masking(mask_value=0)(gru_input)
-    v = keras.layers.Activation('tanh')(mask)
-    gru_out = keras.layers.GRU(gru_dimentions, return_sequences=True, dropout=0.5)(v)
+    emb = keras.layers.Dense(128, activation='relu', kernel_initializer=keras.initializers.constant(diagcode_emb), name='diagcode_emb')(mask)
+    gru_out = keras.layers.GRU(gru_dimentions, return_sequences=True, dropout=0.5)(emb)
 
-    tree_input = keras.layers.Input((tree.shape[1], tree.shape[2], tree.shape[3]), name='tree_input')
-    mask1 = keras.layers.Masking(mask_value=0)(tree_input)
-    context_vector = KAMEAttention(units=gru_dimentions)([mask1, gru_out])
-    s = keras.layers.concatenate([gru_out, context_vector], axis=-1)
-    main_output = keras.layers.Dense(labelCount, activation='softmax', name='main_output')(s)
+    tree_input = keras.layers.Input((tree.shape[1], tree.shape[2]), name='tree_input')
+    tree_mask = keras.layers.Masking(mask_value=0)(tree_input)
+    tree_emb = keras.layers.Dense(128, activation='relu', kernel_initializer=keras.initializers.constant(knowledge_emb), name='knowledge_emb')(tree_mask)
+
+    context_vector, weights = ScaledDotProductAttention(output_dim=128)([tree_emb, gru_out])
+    st = keras.layers.concatenate([gru_out, context_vector], axis=-1)
+
+    main_output = keras.layers.Dense(labelCount, activation='softmax', name='main_output')(st)
 
     model = keras.models.Model(inputs=[gru_input, tree_input], outputs=main_output)
 
-    model.summary()
-    # checkpoint = tf.keras.callbacks.ModelCheckpoint(
-    #     filepath='G:\\模型训练保存\\KAME_' + str(gru_dimentions) + '_dropout\\rate05\\model_{epoch:02d}', save_freq='epoch')
 
     callback_history = metricsHistory()
     callback_lists = [callback_history]
+    model.summary()
     model.compile(optimizer='adam', loss='binary_crossentropy')
 
     history = model.fit([x, tree], y,
